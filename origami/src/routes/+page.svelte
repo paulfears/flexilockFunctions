827bd9201d2f94e588123d1dc7e8a3a14326fd38
paul fears <paulrfears@gmail.com>
2024-02-24 17:17:55 -0600

    here

--- origami/src/routes/+page.svelte
+++ origami/src/routes/+page.svelte
@@ -2,14 +2,7 @@
 <script lang='ts'>
 	import { browser } from '$app/environment';
 	if(browser){
-		window.handleCredentialResponse = function(googleUser) {
-			var profile = googleUser.getBasicProfile();
-			console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-			console.log('Name: ' + profile.getName());
-			console.log('Image URL: ' + profile.getImageUrl());
-			console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-			return true;
-		}
+
 	}
   
   </script>

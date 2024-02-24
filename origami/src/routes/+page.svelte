ee0e9e631751e76677849050c2fe52aa123be347
paul fears <paulrfears@gmail.com>
2024-02-24 16:54:15 -0600

    signon

--- origami/src/routes/+page.svelte
+++ origami/src/routes/+page.svelte
@@ -1,12 +1,24 @@
-<script>
-</script>
 
+<script>
+
+	window.onSignIn = function(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+  
+  </script>
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>Hello world</h1>
+	
+	<p>Hello world</p>
+	<p>verify page</p>
+	<div class="g-signin2" data-onsuccess="onSignIn"></div>
 </section>
 

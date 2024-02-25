771f5bbde2b7b22dc87efceffe74e1386927b97e
paul fears <paulrfears@gmail.com>
2024-02-24 19:18:58 -0600

    data

---
+++ origami/netlify/functions/handleLogin.ts
@@ -0,0 +1,24 @@
+import {Handler, HandlerEvent, HandlerContext, HandlerResponse} from "@netlify/functions"
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    // your server-side functionality
+    console.log("event is ");
+    console.log(event);
+    console.log("context is ");
+    console.log(context);
+    console.log("event body is");
+    console.log(event.body);
+    console.log("here");
+    const json_data = {
+      "hello":"world",
+      "postData": event.body
+    }
+      return {'statusCode': 200,
+          'headers': {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": 'GET, POST, PUT, DELETE, OPTIONS'
+          },
+          'body': JSON.stringify(json_data)
+      }    
+  };
+  
+  export { handler };
